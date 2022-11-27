506a8b5593ada78e06a83c8bfc41200d990703d1
Johana Valencia <115668043+JohaValencia@users.noreply.github.com>
2022-11-26 19:15:37 -0500

    Creación de nueva rama y actualización del proyecto

--- src/test/java/script/tests/AccountNumberTest.java
+++ src/test/java/script/tests/AccountNumberTest.java
@@ -4,16 +4,24 @@ import bankTransaction.Reporter;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import script.pojo.User;
 import script.utils.BaseTest;
 
 public class AccountNumberTest extends BaseTest {
 
     private int id = 1;
 
-    @Parameters({"endpoint"})
+
     @Test
+    @Parameters("endpoint")
     public void accountNumberTest(String endpoint) {
+        User userToUpdate = createRandomUser();
+        userToUpdate = newUser(endpoint, userToUpdate);
+        userToUpdate.setAccountNumber(666);
+        System.out.println(updateUser(endpoint, userToUpdate));
+        User userFromAPI =  getUser(endpoint, userToUpdate.getId());
+
         Reporter.info("Checking the account number was correctly updated");
-        Assert.assertEquals(userIsUpdate(endpoint, id), 200, "User is not updated");
+        Assert.assertEquals(userFromAPI.getAccountNumber(), 666, "User is not updated");
     }
 }
